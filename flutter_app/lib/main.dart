e6ca781db322dd312da92acab5b4772e565b5e5a
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-12-29 18:37:12 +0400

    import material.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,14 +1,5 @@
-// void main() {
-//   for (int i = 0; i < 5; i++) {
-//     print('hello ${i + 1}');
-//   }
-// }
-addNumbers(double num1, double num2) {
-//   print(num1+num2);
-  return num1 + num2;
-}
+import 'package:flutter/material.dart';
 
-void main() {
-  print(addNumbers(1, 2.6));
-  print('hello');
-}
+void main() {}
+
+class MyApp extends StatelessWidget {}
