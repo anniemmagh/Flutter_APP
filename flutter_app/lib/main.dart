53e87ccb954186c11855d4a2db8513d0e9cbce12
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-12-29 15:23:58 +0400

    dart double types

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,14 @@
+// void main() {
+//   for (int i = 0; i < 5; i++) {
+//     print('hello ${i + 1}');
+//   }
+// }
+addNumbers(double num1, double num2) {
+//   print(num1+num2);
+  return num1 + num2;
+}
+
 void main() {
-  for (int i = 0; i < 5; i++) {
-    print('hello ${i + 1}');
-  }
+  print(addNumbers(1, 2.6));
+  print('hello');
 }
